227d8df177d8e1249d33af719dd9d84b2c51770b
ManiSudate <manisudate25@gmail.com>
2026-01-20 16:45:16 +0100

    prova

--- Tombolaporcodue/src/Main.java
+++ Tombolaporcodue/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.out.println("\nEstratto: " + numero);
 
             cartella.controllaNumero(numero);
-            cartella.stampa();
+            
         }
     }
 }
